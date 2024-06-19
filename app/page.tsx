6f68c3397ac87cfefcde994d1b1e37373697a54f
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-19 21:26:42 +0700

    all

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
                   width: '50px',
                   height: 'auto'
               }}
-              className='mb-5'
+              className='mb-10'
           />
           <img 
               src="https://ik.imagekit.io/vtvggda66/logotype%20alfath.svg?updatedAt=1718804247754" 
@@ -24,9 +24,9 @@ const page = () => {
                   width: '40%',
                   height: 'auto'
               }}
-              className='my-2'
+              className='mb-5'
           />
-          <div className="font-bold text-base text-slate-200 font-Montserrat my-5 tracking-tight py-2 px-4 rounded-lg bg-[#0f7ba5] inline-block">
+          <div className="font-bold text-base text-slate-200 font-Montserrat my-5 tracking-tight py-2 px-4 rounded-lg bg-[#0f7ba5] inline-block shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
               Software Company
           </div>
           
@@ -38,16 +38,18 @@ const page = () => {
           </div>
       </div>
 
-      <div className='bg-[#00adef] min-h-screen w-full flex items-center flex-col relative pt-32'>
+      <div className='bg-white min-h-screen w-full flex items-center flex-col relative pt-32'>
 
-        <div className="columns-1 gap-4">
-          {[24, 32, 36, 32, 32, 32, 16, 16, 64].map((height, index) => (
-            <div
-              key={index}
-              className={`mb-4 h-${height} break-inside-avoid rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900`}
-            />
-          ))}
-        </div>
+          <div className="grid auto-rows-[192px] grid-cols-3 gap-4">
+            {[...Array(7)].map((_, i) => (
+              <div
+                key={i}
+                className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${
+                  i === 3 || i === 6 ? "col-span-2" : ""
+                }`}
+              ></div>
+            ))}
+          </div>
 
       </div>
 
