cec2a4d016206b9691c50153a7f6b1407cc1c97a
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-19 19:18:10 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ const page = () => {
   return (
     <>
         <div className='bg-[#00adef] min-h-screen w-full flex items-center justify-center'>
-            <div className="font-bold text-white">
+            <div className="font-bold text-white mr-2 text-lg">
                 ALFATH INNOVTAIVE
             </div>
-            <div className="font-bold text-white">
+            <div className="font-bold text-white text-sm text-red-700">
                 Coming Soon!
             </div>
         </div>
