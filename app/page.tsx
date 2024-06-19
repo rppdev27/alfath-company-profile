410dceb8edd853bcc5600a8fcd3ade610e4fa61a
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-19 20:57:31 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,37 @@ import React from 'react'
 const page = () => {
   return (
     <>
-        <div className='bg-[#00adef] min-h-screen w-full flex items-center justify-center flex-col'>
-            <img 
+        <div className='bg-[#00adef] min-h-screen w-full flex items-center justify-center flex-col relative'>
+          <img 
               src="https://ik.imagekit.io/vtvggda66/logogram%20alfath.svg?updatedAt=1718804248063" 
               alt="alfath innovative"
               style={{
-                width: '50px',
-                height: 'auto'
+                  width: '50px',
+                  height: 'auto'
               }}
-              className='my-2'
-            />
-            <img 
+              className='mb-[100px]'
+          />
+          <img 
               src="https://ik.imagekit.io/vtvggda66/logotype%20alfath.svg?updatedAt=1718804247754" 
               alt="alfath innovative"
               style={{
-                width: '400px',
-                height: 'auto'
+                  width: '65%',
+                  height: 'auto'
               }}
               className='my-2'
-            />
-            <div className="font-bold text-base text-slate-200 font-Montserrat my-2 tracking-tight p-2 rounded-md bg-[#0f7ba5] inline-block">
-                Software Company
-            </div>
-            
-            <div className="font-bold text-base text-slate-200 font-Jost">
-                Coming Soon!
-            </div>
-        </div>
+          />
+          <div className="font-bold text-base text-slate-200 font-Montserrat my-5 tracking-tight py-2 px-3 rounded-md bg-[#0f7ba5] inline-block">
+              Software Company
+          </div>
+          
+          <div className="font-bold text-base text-slate-200 font-Jost my-3">
+              Coming Soon!
+          </div>
+
+          <div className="absolute bottom-0 w-full h-1/4 bg-cover bg-no-repeat" style={{ backgroundImage: 'url(https://ik.imagekit.io/vtvggda66/peopleillust.svg?updatedAt=1718805343633)' }}>
+          </div>
+      </div>
+
     </>
   )
 }
