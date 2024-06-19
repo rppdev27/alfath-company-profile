371913ed81d7ba92d0ab5a6f795ecbff5f92b1e5
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-19 19:16:08 +0700

    ALL

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ const page = () => {
     <>
         <div className='bg-[#00adef] min-h-screen w-full flex items-center justify-center'>
             <div className="font-bold text-white">
+                ALFATH INNOVTAIVE
+            </div>
+            <div className="font-bold text-white">
                 Coming Soon!
             </div>
         </div>
