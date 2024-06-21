a8585c081b5fe1767001855764fce0bc682564d0
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-21 15:05:35 +0700

    all

--- app/birthday-01/page.tsx
+++ app/birthday-01/page.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
     };
   return (
 
-    <div className="flex justify-center items-center bg-slate-200 relative contt">
+    <div className="flex justify-center items-center bg-slate-200 relative">
         <div className="bg-cover sm:bg-contain bg-no-repeat min-h-screen flex justify-center items-center relative w-[450px] bg-center mx-auto z-10 overflow-x-hidden overflow-y-hidden" 
             style={{ 
                 backgroundImage: `url(https://terhubung.id/wp-content/uploads/01.-BG-FIXED.jpeg)`,
@@ -25,35 +25,35 @@ const App: React.FC = () => {
                 backgroundAttachment: 'fixed'
             }}
         >
-        <div className="flex justify-center items-center min-h-screen text-white"
+        <div className="flex justify-center items-center min-h-screen text-white w-full p-6"
             style={{
                 fontFamily: 'Londrina Solid'
             }}
         >
-           <div className="bg-transparent bg-opacity-80 w-full mx-auto rounded shadow-md">
-            <section className="mb-4 p-4 bg-transparent rounded shadow h-screen flex flex-col justify-center">
+           <div className="bg-transparent bg-opacity-80 w-full mx-auto shadow-md">
+            <section className="p-4 bg-transparent h-screen flex flex-col justify-center bg-blue-950 shadow-md bg-opacity-70 w-full rounded-lg mb-10">
                 <h2 className="text-lg font-semibold">Section 1</h2>
                 <p>This is the content of the first section.</p>
                 <button
                     onClick={scrollToSection2}
-                    className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                    className="mt-4 px-4 py-2 bg-slate-300 text-blue-800 rounded hover:bg-slate-400"
                 >
                     Go to Section 2
                 </button>
             </section>
             {showSections && (
                 <>
-                    <section id="section2" className="mb-4 p-4 bg-transparent rounded shadow h-screen flex flex-col justify-center">
+                    <section id="section2" className="p-4 bg-transparent h-screen flex flex-col justify-center bg-blue-950 shadow-md bg-opacity-70 w-full rounded-lg  mb-10">
                         <h2 className="text-lg font-semibold">Section 2</h2>
                         <p>This is the content of the second section.</p>
                     </section>
-                    <section className="mb-4 p-4 bg-transparent rounded shadow h-screen flex flex-col justify-center">
+                    <section className="mb-4 p-4 bg-transparent rounded shadow h-screen flex flex-col justify-center bg-blue-950 shadow-md bg-opacity-70 w-full rounded-lg  mb-10">
                         <h2 className="text-lg font-semibold">Section 3</h2>
                         <p>This is the content of the third section.</p>
                     </section>
                 </>
             )}
-        </div>
+            </div>
         </div>
         </div>
 
