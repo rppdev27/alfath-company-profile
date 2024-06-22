97013f8960426b402e036b63a8efa7cfcf1218e3
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-23 03:40:11 +0700

    Update page.tsx

--- app/birthday-lebah/page.tsx
+++ app/birthday-lebah/page.tsx
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 import { FaRegEnvelopeOpen } from "react-icons/fa6";
 import { TbMapSearch } from "react-icons/tb";
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
+import { FaRegCircleDot } from "react-icons/fa6";
 
 const App: React.FC = () => {
 
@@ -61,31 +62,31 @@ const App: React.FC = () => {
             }}
         >
         {
-            showSections && <div className="bg-black fixed right-1 rounded-lg shadow-lg bg-opacity-50 top-1/2 transform -translate-y-1/2 flex justify-center h-auto w-16 text-white flex-col z-30 p-0 items-center py-1 h-auto"
+            showSections && <div className="bg-black fixed right-1 rounded-lg shadow-lg bg-opacity-50 top-1/2 transform -translate-y-1/2 flex justify-center px-1 text-white flex-col z-30 p-0 items-center py-1 h-auto"
             style={{
                 fontFamily: 'Londrina Solid',
                 fontSize: '0.8rem'
             }}
         >
-            <button className={`text-[0.5rem] w-full uppercase items-center py-2 px-1 sm:px-4 mb-4 flex flex-col ${currentSection === 'section1' ? 'bg-[#9B692C]' : ''}`} onClick={() => scrollToSection('section1')}>
-                <MdOutlineKeyboardDoubleArrowDown size={15}/>
-                Beranda
+            <button className={`text-[0.5rem] w-full uppercase items-center py-2 mb-4 px-2 flex flex-col ${currentSection === 'section1' ? 'text-[#6fff6f]' : ''}`} onClick={() => scrollToSection('section1')}>
+                <FaRegCircleDot size={15}/>
+                {/* Beranda */}
             </button>
-            <button className={`text-[0.5rem] w-full uppercase items-center flex flex-col transition-all duration-300 py-2 px-1 sm:px-4 mb-4 ${currentSection === 'section2' ? 'bg-[#9B692C]' : ''}`} onClick={() => scrollToSection('section2')}>
-                <MdOutlineKeyboardDoubleArrowDown size={15}/>
-                Beranda
+            <button className={`text-[0.5rem] w-full uppercase items-center flex flex-col transition-all duration-300 py-2 mb-4 ${currentSection === 'section2' ? 'text-[#6fff6f]' : ''}`} onClick={() => scrollToSection('section2')}>
+                <FaRegCircleDot size={15}/>
+                {/* Beranda */}
             </button>
-            <button className={`text-[0.5rem] w-full uppercase flex items-center flex-col transition-all duration-300 py-2 px-1 sm:px-4 mb-4 ${currentSection === 'section3' ? 'bg-[#9B692C]' : ''}`} onClick={() => scrollToSection('section3')}>
-                <MdOutlineKeyboardDoubleArrowDown size={15}/>
-                Beranda
+            <button className={`text-[0.5rem] w-full uppercase flex items-center flex-col transition-all duration-300 py-2 mb-4 ${currentSection === 'section3' ? 'text-[#6fff6f]' : ''}`} onClick={() => scrollToSection('section3')}>
+                <FaRegCircleDot size={15}/>
+                {/* Beranda */}
             </button>
-            <button className={`text-[0.5rem] w-full uppercase flex items-center flex-col transition-all duration-300 py-2 px-1 sm:px-4 mb-4 ${currentSection === 'section4' ? 'bg-[#9B692C]' : ''}`} onClick={() => scrollToSection('section4')}>
-                <MdOutlineKeyboardDoubleArrowDown size={15}/>
-                Beranda
+            <button className={`text-[0.5rem] w-full uppercase flex items-center flex-col transition-all duration-300 py-2 mb-4 ${currentSection === 'section4' ? 'text-[#6fff6f]' : ''}`} onClick={() => scrollToSection('section4')}>
+                <FaRegCircleDot size={15}/>
+                {/* Beranda */}
             </button>
-            <button className={`text-[0.5rem] w-full uppercase flex items-center flex-col transition-all duration-300 py-2 px-1 sm:px-4 ${currentSection === 'section5' ? 'bg-[#9B692C]' : ''}`} onClick={() => scrollToSection('section5')}>
-            <MdOutlineKeyboardDoubleArrowDown size={15}/>
-                Beranda
+            <button className={`text-[0.5rem] w-full uppercase flex items-center flex-col transition-all duration-300 py-2 ${currentSection === 'section5' ? 'text-[#6fff6f]' : ''}`} onClick={() => scrollToSection('section5')}>
+            <FaRegCircleDot size={15}/>
+                {/* Beranda */}
             </button>
         </div>
         }
