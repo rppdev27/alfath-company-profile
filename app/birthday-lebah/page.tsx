9e5a2074f165e9cb6fdde84033ddb9615d63c77a
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-06-23 02:52:18 +0700

    Update page.tsx

--- app/birthday-lebah/page.tsx
+++ app/birthday-lebah/page.tsx
@@ -220,7 +220,7 @@ const App: React.FC = () => {
                         }}/>
                     </div>
 
-                    <section id="section4" className="p-4 h-[calc(100vh-80px)] flex flex-col justify-start shadow-md bg-[#F0A038] bg-opacity-80 w-full rounded-lg mb-10">
+                    <section id="section4" className="p-4 h-auto sm:h-[calc(100vh-80px)] flex flex-col justify-start shadow-md bg-[#F0A038] bg-opacity-80 w-full rounded-lg mb-10">
                         
                         <div className="text-[#9B692C] text-3xl text-center my-6 uppercase">
                             Galeri
@@ -247,20 +247,20 @@ const App: React.FC = () => {
 
 
 
-                    <section id="section4" className="p-4 min-h-[calc(100vh-80px)] h-auto flex flex-col justify-start shadow-md bg-[#F0A038] bg-opacity-80 w-full rounded-lg mb-10">
+                    <section id="section4" className="p-4 h-auto sm:h-[calc(100vh-80px)] flex flex-col justify-start shadow-md bg-[#F0A038] bg-opacity-80 w-full rounded-lg mb-10">
                         
                         <div className="text-[#9B692C] text-3xl text-center my-6 uppercase">
                             Ucapan & Doa
                         </div>
                         <div className="mx-auto flex items-center justify-center border-white border-solid border-2 h-12 w-12 bg-cyan-500 rounded-full shadow-lg cursor-pointer hover:bg-cyan-700 animate-bounce mt-4"
-                        onClick={()=> scrollToSection('section6f')}
-                    >
-                        <MdOutlineKeyboardDoubleArrowDown size={25} className="h-6 w-6 text-white" />
-                    </div>
+                            onClick={()=> scrollToSection('section6f')}
+                        >
+                            <MdOutlineKeyboardDoubleArrowDown size={25} className="h-6 w-6 text-white" />
+                        </div>
 
                     </section>
 
-                    <section id="section5" className="p-4 min-h-[calc(100vh-80px)] h-auto flex flex-col justify-start shadow-md bg-[#F0A038] bg-opacity-80 w-full rounded-lg mb-10">
+                    <section id="section5" className="p-4 h-auto sm:h-[calc(100vh-80px)] flex flex-col justify-start shadow-md bg-[#F0A038] bg-opacity-80 w-full rounded-lg mb-10">
                         <img src="https://terhubung.id/wp-content/uploads/05.-HONEY-BEE-03.png" alt="Astronaut" className="mx-auto"/>
                         <div className="text-[#9B692C] text-3xl text-center my-6 uppercase">
                             Terima Kasih
