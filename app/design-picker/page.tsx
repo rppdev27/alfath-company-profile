3cf3be529169bbd264d0c66454baf68a7cfa4bd3
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-03 14:44:49 +0700

    Update page.tsx

--- app/design-picker/page.tsx
+++ app/design-picker/page.tsx
@@ -94,15 +94,22 @@ const Desain = () => {
         {isOpen ? 'Close' : 'Open'}
       </button>
             {/* this is collapsible menu */}
+            <button
+        onClick={togglePanel}
+        className="fixed z-50 p-2 text-[0.4rem] bg-white rounded-sm shadow-lg text-slate-500 top-2 right-4 hover:bg-slate-300"
+      >
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+            {/* this is collapsible menu */}
             <div className={`fixed z-50 flex flex-col items-center text-[0.6rem] p-3 transition-all duration-500 bg-white rounded-sm top-8 right-0 h-screen-minus-80 overflow-scroll my-4
               transform ${
                 isOpen ? 'translate-x-0' : 'translate-x-full'
               } transition-transform duration-300 ease-in-out`}
             >
 
-                <div className='w-full p-2 my-2 text-white rounded-md bg-slate-600'>
+                {/* <div className='w-full p-2 my-2 text-white rounded-md bg-slate-600'>
                   Close Menu
-                </div>
+                </div> */}
 
                 <div className='my-2'>
                   Template List
@@ -111,29 +118,48 @@ const Desain = () => {
                 {
                   template_list.map((template_, index) => (
                     <>
-                    <div 
-                      key={index}
-                      onClick={() => setTemplate(template_.id)} 
-                      className='hover:bg-slate-200 transition-all duration-500 p-2 my-1 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] cursor-pointer'>
-                      
-                      <img src={template_.thumbnail} alt='undangan digital'/>
-                      
-                      <div className="p-1">
-                        {template_.name}
-                      </div>
+                          {/* Container div for the template item */}
+                          <div 
+                            key={index}
+                            onClick={() => setTemplate(template_.id)} 
+                            className='max-w-[150px] 
+                            h-auto 
+                            z-50 
+                            hover:bg-slate-200 
+                            transition-all 
+                            duration-500 
+                            p-2 
+                            shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] 
+                            cursor-pointer
+                            mb-4
+                            '
+                          >
+                            <img src={template_.thumbnail} alt='undangan digital'/>
+                            
+                            <div className="p-1">
+                              {template_.name}
+                            </div>
 
-                    </div>
-                    <div 
-                      key={index}
-                      onClick={() => setTemplate(template_.id)} 
-                      className='hover:bg-slate-200 transition-all duration-500 p-2 my-1 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] cursor-pointer'>
-                      
-                      <div className="p-1">
-                        Color List
-                      </div>
 
-                    </div>
-                    </>
+                            <div className="mb-1">
+                              Warna
+                            </div>
+
+                            {/* Scrollable section for the template list */}
+                            <div className="flex flex-row w-auto p-2 overflow-x-auto">
+                              {pastelColors.map((item, idx) => (
+                                <div
+                                  key={idx}
+                                  className='w-[100px] h-auto p-4 rounded-md shadow-md mb-1 mr-3'
+                                  style={{
+                                    backgroundColor: item.hex,
+                                  }}
+                                />
+                              ))}
+                            </div>
+                          </div>
+                        </>
+
                     
                   ))
                 }
