f9beddafdecfcea5782b4e7e9175e797334d0424
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-03 22:08:44 +0700

    Update page.tsx

--- app/design-picker/page.tsx
+++ app/design-picker/page.tsx
@@ -206,6 +206,15 @@ const Desain = () => {
   return (
     <div className='relative'>
 
+<div className="fixed bottom-0 left-0 right-0 bg-white p-4 flex rounded-md justify-around items-center flex-row shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]">
+
+<div className="">Template</div>
+<div className="">Warna</div>
+
+    
+</div>
+
+
       {
 
         isOpen ? <></> : <button
@@ -220,13 +229,13 @@ const Desain = () => {
       
 
             {/* this is collapsible menu */}
-            <div className={`fixed z-50 flex flex-col items-center text-[0.6rem] p-3 transition-all duration-500 bg-white rounded-sm top-8 right-0 h-screen-minus-80 overflow-scroll my-4
+            <div className={`fixed z-50 flex flex-col items-center text-[0.6rem] p-3 transition-all duration-500 bg-white rounded-sm top-2 right-0 h-screen-minus-80 overflow-scroll my-4
               transform ${
                 isOpen ? 'translate-x-0' : 'translate-x-full'
               } transition-transform duration-300 ease-in-out`}
             >
 
-                <div className='w-full p-2 my-2 text-white rounded-md bg-slate-600 shadow-lg' onClick={togglePanel}>
+                <div className='cursor-pointer w-full p-2 my-2 text-white rounded-md bg-slate-600 shadow-lg' onClick={togglePanel}>
                   Close
                 </div>
 
