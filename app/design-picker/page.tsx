d4302c27a4648e0d75eeea04a85318a8544e8ec0
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-04 13:55:44 +0700

    Update page.tsx

--- app/design-picker/page.tsx
+++ app/design-picker/page.tsx
@@ -424,7 +424,7 @@ const Desain = () => {
             <div className="flex flex-col w-full h-full text-xs transition-all duration-500">
                 {activeTab === 'tanya' ? (
                     <div className="bg-white p-4 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
-                        <h2 className="text-base font-bold">Tanya tentang</h2>
+                        <h2 className="text-base font-bold">Topik Pertanyaan</h2>
                         <div className="flex flex-col gap-4">
                             <input type="radio" name="type" value="Desain" onChange={handleOptionChange} />
                             <label htmlFor="Desain">Desain</label>
