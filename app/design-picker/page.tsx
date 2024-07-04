339797d05c2c6ecb9ac822c5a6a7595b5c67bdfc
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-04 11:16:36 +0700

    Update page.tsx

--- app/design-picker/page.tsx
+++ app/design-picker/page.tsx
@@ -343,7 +343,8 @@ const Desain = () => {
 
 <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 w-auto py-1 px-3 flex text-[0.5rem] sm:text-[0.6rem] tracking-tight text-white rounded-full frosted-glass hover:bg-opacity-40 font-bold"
       style={{
-        zIndex: '999'
+        zIndex: '999',
+        left: isOpen ? '25%' : '50%'
       }}
 >
 
