0e63abfd531a99b339e48745fbf09f2fe8a7ce70
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-04 10:01:26 +0700

    Update page.tsx

--- app/design-picker/page.tsx
+++ app/design-picker/page.tsx
@@ -363,15 +363,15 @@ const Desain = () => {
       
 
             {/* this is collapsible menu */}
-            <div className={`fixed z-50 flex flex-col items-center text-[0.6rem] p-3 transition-all duration-500 bg-white rounded-sm top-2 right-0 h-screen-minus-80 transform ${
+            <div className={`fixed z-50 flex flex-col items-center text-[0.6rem] p-3 transition-all duration-500 bg-transparent border border-white text-white rounded-full backdrop-filter backdrop-blur-md bg-opacity-20 rounded-sm top-2 my-1 right-0 sm:h-screen h-screen-minus-80 transform ${
                 isOpen ? 'translate-x-0' : 'translate-x-full'
               } transition-transform duration-300 ease-in-out`}>
-  <div className="relative w-full">
-    <div className='sticky top-0 cursor-pointer w-full p-2 mb-2 text-slate-700 rounded-md bg-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] flex flex-row justify-between items-center' onClick={togglePanel}>
-      <IoCloseCircle size={20} className='mr-1 text-red-900'/>
-      Daftar Template
-    </div>
-  </div>
+            <div className="relative w-full">
+              <div className='sticky top-0 cursor-pointer w-full p-2 mb-2 text-slate-700 rounded-md bg-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] flex flex-row justify-between items-center' onClick={togglePanel}>
+                <IoCloseCircle size={20} className='mr-1 text-red-900'/>
+                Daftar Template
+              </div>
+            </div>
                 
 
                 {/* <div className='my-1'>
@@ -446,6 +446,7 @@ const Desain = () => {
                     
                   ))
                 }
+
                 </div>
           </div>
 
