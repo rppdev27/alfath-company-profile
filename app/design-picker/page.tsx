03c3e18d70bfb7061a60f2467d5af24ca344d116
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-04 10:46:31 +0700

    all

--- app/design-picker/page.tsx
+++ app/design-picker/page.tsx
@@ -461,10 +461,11 @@ const Desain = () => {
                               shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] 
                               cursor-pointer
                               mb-4
+                              ${backgroundColor: index === isActiveTemplate ? 'grey' : 'frosted-glass'}
                             `}
-                            style={{
-                              backgroundColor: index === isActiveTemplate ? 'grey' : 'white'
-                            }}
+                            // style={{
+                            //   backgroundColor: index === isActiveTemplate ? 'grey' : 'white'
+                            // }}
                           >
                             <img src={template_.thumbnail} alt='undangan digital'/>
                             
