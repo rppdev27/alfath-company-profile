68c7bb209a8e6e3af5956b8ceba0e63e00a9930b
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-05 22:53:03 +0700

    Update page.tsx

--- app/miniLP/page.tsx
+++ app/miniLP/page.tsx
@@ -4,6 +4,7 @@ import React,{ useEffect } from 'react';
 import AOS from 'aos'
 import 'aos/dist/aos.css';
 import { MdOutlineImageSearch } from "react-icons/md";
+import { CgTemplate } from "react-icons/cg";
 
 
 function LandingPage() {
