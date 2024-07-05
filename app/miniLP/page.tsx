1bbebb00bde23e453e1f0fea60b66e4559fa3884
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-05 20:11:15 +0700

    Update page.tsx

--- app/miniLP/page.tsx
+++ app/miniLP/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React,{ useEffect } from 'react';
 import AOS from 'aos'
 import 'aos/dist/aos.css';
 import { MdOutlineImageSearch } from "react-icons/md";
