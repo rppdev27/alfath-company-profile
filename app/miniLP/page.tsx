742724ae2f7c2cb3b86c9697f912c51d4e2c944b
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-05 20:04:15 +0700

    Update page.tsx

--- app/miniLP/page.tsx
+++ app/miniLP/page.tsx
@@ -23,12 +23,12 @@ function LandingPage() {
             height: 'auto',
           }}
         />
-        <h1 className="text-4xl tracking-tight font-bold mb-4"
+        <h1 className="text-4xl tracking-tight font-extrabold my-6 text-white"
           style={{
             fontFamily: 'Damion'
           }}
-        >Luxury Wedding Invitation</h1>
-        <button className="p-5 font-semibold bg-green-700 hover:bg-green-800 text-white text-md py-2 px-4 rounded shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+        >Your Luxury Wedding Invitation</h1>
+        <button className="p-5 font-semibold bg-green-700 hover:bg-green-800 text-white text-md py-2 px-4 rounded-lg shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
           Explore Design
         </button>
       </div>
