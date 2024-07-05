1e89b9d0e43ad096b95b512f845dcd08e9e9e88d
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-05 20:05:19 +0700

    Update page.tsx

--- app/miniLP/page.tsx
+++ app/miniLP/page.tsx
@@ -1,8 +1,17 @@
 'use client'
 
 import React from 'react';
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
 
 function LandingPage() {
+
+  useEffect(() => {
+        AOS.init();
+  }, [])
+
+
   return (
     <div
       className="flex flex-col items-center justify-center h-screen w-screen bg-gray-100"
@@ -22,13 +31,17 @@ function LandingPage() {
             width: '175px',
             height: 'auto',
           }}
+          data-aos='fade-down'
         />
         <h1 className="text-4xl tracking-tight font-extrabold my-6 text-white"
           style={{
             fontFamily: 'Damion'
           }}
+          data-aos='fade-down'
         >Your Luxury Wedding Invitation</h1>
-        <button className="p-5 font-semibold bg-green-700 hover:bg-green-800 text-white text-md py-2 px-4 rounded-lg shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+        <button 
+          data-aos='fade-down'
+          className="p-5 font-semibold bg-green-700 hover:bg-green-800 text-white text-md py-2 px-4 rounded-lg shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
           Explore Design
         </button>
       </div>
