222950dbdeff26ff6c7ee2b8fe9619e3bbec7e05
rppdev27 <149379969+rppdev27@users.noreply.github.com>
2024-07-06 11:05:17 +0700

    Update DesignPicker.tsx

--- components/DesignPicker.tsx
+++ components/DesignPicker.tsx
@@ -478,7 +478,7 @@ useEffect(() => {
         </div>
       )}
 
-<div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 w-auto py-1 px-3 flex text-[0.5rem] sm:text-[0.6rem] tracking-tight text-white rounded-full yellow-bg hover:bg-opacity-40 font-bold transition-all duration-500"
+<div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 w-auto py-1 px-3 flex text-[0.5rem] sm:text-[0.6rem] tracking-tight text-white rounded-full frosted-glass hover:bg-opacity-40 font-bold transition-all duration-500"
       style={{
         zIndex: '999',
         left: isOpen ? '80px' : '50%'
